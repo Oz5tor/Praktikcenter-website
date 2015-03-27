88fbfffcfc467e59bd8465d2b6ffa168a1bffe7f
oz5tor <torsoya@gmail.com>
2015-03-27 10:07:16 +0100

    Cores site Started
    the building of hte core parts of site has begun
    
    the global Database connec variable is $db_conn

---
+++ Site/DBcon/dbconn.php
@@ -0,0 +1,7 @@
+<?php 
+    $db_host="192.168.0.4"; // Host name
+    $db_username="praktikcenter"; // Mysql username
+    $db_password="bjarne42"; // Mysql password
+    $db_name="praktikcenter"; // Database name
+    $db_conn = mysqli_connect("$db_host","$db_username","$db_password","$db_name");
+?>
