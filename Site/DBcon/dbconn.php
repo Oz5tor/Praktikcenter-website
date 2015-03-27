f85a16cc0769810331aa3c3d3b2222d56f8233fc
oz5tor <torsoya@gmail.com>
2015-03-27 10:09:02 +0100

    Revert "Cores site Started"
    This reverts commit 88fbfffcfc467e59bd8465d2b6ffa168a1bffe7f.

--- Site/DBcon/dbconn.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $db_host="192.168.0.4"; // Host name
-    $db_username="praktikcenter"; // Mysql username
-    $db_password="bjarne42"; // Mysql password
-    $db_name="praktikcenter"; // Database name
-    $db_conn = mysqli_connect("$db_host","$db_username","$db_password","$db_name");
-?>
--- Site/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-ob_start();
-session_start();
-require_once("/DBcon/dbconn.php");
-include_once("/Login/login_index.php");
-echo '<pre>';
-echo print_r($_SESSION);
-echo '</pre>';
-?>
