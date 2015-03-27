ecfd2465979179588e6f795f186b2e5640dc9409
oz5tor <torsoya@gmail.com>
2015-03-27 10:17:38 +0100

    in apopaiate folder name
    changed name ofthe module foulder so its able to fit more of the user
    area, like alle the CRUD instead of only Read

--- Site/index.php
+++ Site/index.php
@@ -2,7 +2,7 @@
 ob_start();
 session_start();
 require_once("/DBcon/dbconn.php");
-include_once("/Login/login_index.php");
+include_once("/Login/login.php");
 echo '<pre>';
 echo print_r($_SESSION);
 echo '</pre>';
