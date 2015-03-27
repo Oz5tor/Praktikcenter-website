768d0c64f59f731462ad517fa96af9d4b2e3425b
oz5tor <torsoya@gmail.com>
2015-03-27 13:29:41 +0100

    forgot to change include after changing folder to User_molue

--- Site/index.php
+++ Site/index.php
@@ -2,7 +2,7 @@
 ob_start();
 session_start();
 require_once("/DBcon/dbconn.php");
-include_once("/Login/login.php");
+include_once("/User_module/login.php");
 echo '<pre>';
 echo print_r($_SESSION);
 echo '</pre>';
