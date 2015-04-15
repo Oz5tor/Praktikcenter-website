0c9d78f26bb3e17457368b9b2bf1d7936306ffce
oz5tor <torsoya@gmail.com>
2015-04-15 12:40:36 +0200

    wrong file route
    route to DBconn and user login was wrong now corrected

--- Site/index.php
+++ Site/index.php
@@ -1,8 +1,8 @@
 <?php
 ob_start();
 session_start();
-require_once("/DBcon/dbconn.php");
-include_once("/User_module/login.php");
+require_once("DBcon/dbconn.php");
+include_once("User_module/login.php");
 echo '<pre>';
 echo print_r($_SESSION);
 echo '</pre>';
