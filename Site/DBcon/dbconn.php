502c6d360bdd4b938a1535c84e23b19eb915fc39
oz5tor <torsoya@gmail.com>
2015-04-15 12:39:48 +0200

    using new database
    connection information has to be changed since we got a new server

--- Site/DBcon/dbconn.php
+++ Site/DBcon/dbconn.php
@@ -1,8 +1,8 @@
 <?php 
     $db_host="192.168.0.4"; // Host name
-    $db_username="praktikcenter"; // Mysql username
-    $db_password="bjarne42"; // Mysql password
-    $db_name="praktikcenter"; // Database name
+    $db_username="c1root"; // Mysql username
+    $db_password="A123linux2013"; // Mysql password
+    $db_name="c1praktikcenter"; // Database name
     $db_conn = mysqli_connect("$db_host","$db_username","$db_password","$db_name");
 
 ?>
