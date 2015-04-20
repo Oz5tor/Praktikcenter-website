94f76d4b27d8b859026b0e829cc36b2120d0ba2b
oz5tor <torsoya@gmail.com>
2015-04-20 13:35:04 +0200

    Update to Uder_module
    adde legged_in.php to show infor about the logged in user

---
+++ Site/include/User_module/logged_in.php
@@ -0,0 +1,57 @@
+<?php 
+// ================= Log ud ==========================================
+if (isset($_POST['logud']))
+{
+	session_destroy();
+	header ("location: index.php?side=$side");
+}
+// =========== hvis logget ind =======================================
+
+	$user = $_SESSION['user'];
+	$user_sql = "Select * From user WHERE id = '$user'";
+	$user_result = mysqli_query($db_conn,$user_sql) or die (mysqli_error($db_conn));
+	$user_row = mysqli_fetch_assoc($user_result);
+
+	$title_sql = "Select * From userRoles
+                    Inner Join roles On roles.id = userRoles.roleId
+                    Inner Join user On user.id = userRoles.userId
+                    Where user.id = '$user' Order By userRoles.roleId DESC";
+	$title_result = mysqli_query($db_conn,$title_sql) or die (mysqli_error($db_conn));
+	$user_title = mysqli_fetch_assoc($title_result);
+	
+	if ($user_title['name'] == '')
+	{
+		$title = "Elev";
+	}
+	else
+	{
+		$title = $user_title['name'];
+	}
+	
+?>
+<div>
+<div class="profile_image" style=" padding-right:5px; float:left;height:75px; width:75px;">
+	<img height="75" src="img/profile/<?php echo $user_row['pic']; ?>" />
+</div>
+<div style="float:left">
+<table>
+	<tr>
+    	<td>User:</td>
+        <td><?php echo $user_row['username']; ?> | </td>
+    	<td>Navn:</td>
+        <td><?php echo $user_row['fName'].' '.$user_row['lName']; ?> | </td>
+    	<td>Title:</td>
+        <td><?php echo $title; ?></td>
+    </tr>
+</table>
+</div>
+<div style="float:left">
+<a href="index.php?a=Nyhed">Nyheds administartion <strong>(BETA)</strong></a>
+    <a href="index.php?a=menu">Menu Admin</a>
+<!--<a href="index.php?side=a_menu">Nyheds administartion <strong>(BETA)</strong></a>-->
+</div>
+
+<form action="" method="post">
+<input type="submit" name="logud" value="log ud" />
+</form>
+</div>
---
+++ Site/include/User_module/login.php
@@ -0,0 +1,74 @@
+<?php
+// ================= Log ud ==========================================
+if (isset($_POST['logud']))
+{
+	session_destroy();
+	header ("location: index.php?side=$side");
+}
+// =========================== Log Ind ===============================
+if (isset($_POST['Submit']))
+{
+	$username= strtolower($_POST['username']);
+	$pass = strtolower($_POST['password']);
+	if (($username == '') or ($pass == ''))
+	{
+	// empty apseptere ikke 0 som et gyldit tegn så hvis man kun har er 0 som pass virker det ikke
+		$bo = 'forkert brugernavn eller password';
+	}	
+	else
+		if ((isset($username)) or (isset($pass)))
+	{
+		$username= mysqli_real_escape_string($db_conn,strip_tags ($username));
+		$pass = mysqli_real_escape_string($db_conn,strip_tags ($pass));
+		//$pass = md5($pass);
+        $pass = hash('sha512', $pass);
+		$query="SELECT * FROM user WHERE username ='$username' && password ='$pass'";
+        
+        //$query="SELECT * FROM user WHERE username ='$username' && pass ='$pass'";
+		$result=mysqli_query($db_conn,$query) or die (mysqli_error($db_conn));
+		if (mysqli_num_rows($result)==1)
+		{
+			$row = mysqli_fetch_assoc($result);
+			$user_id = $row['id'];
+			// hvis login lykkes så opretes der $_SESSION's
+			$_SESSION['user']	= $user_id;
+            
+            $roles_sql = "Select * from userRoles WHERE userId = '$user_id'";
+            $roles_result=mysqli_query ($db_conn,$roles_sql) or die (mysqli_error($db_conn));
+            while ($roles_row = mysqli_fetch_assoc($roles_result))
+			{
+                $fk_roleId = $roles_row['roleId'];
+                $perm_id_sql = "SELECT * FROM rolesPermissions WHERE rolesId = '$fk_roleId'";
+                $perm_id_result = mysqli_query ($db_conn,$perm_id_sql) or die (mysqli_error($db_conn));
+                while($perm_id_row = mysqli_fetch_assoc($perm_id_result))
+                {
+                    $perm_id = $perm_id_row['permissionsId'];
+                    $perm_sql = "SELECT * FROM permissions WHERE id ='$perm_id'";
+                    $perm_result = mysqli_query ($db_conn,$perm_sql) or die (mysqli_error($db_conn));
+                    while($perm_row = mysqli_fetch_assoc($perm_result))
+                    {
+                        //echo $perm_row['perName'];
+                        $_SESSION[$perm_row['perName']] = 1;
+                    }
+                    
+                }
+            }
+		} else {
+            $bo = 'Der findes ikke nogen bruger med det password';
+        }
+	}
+}
+// ================ Hvis ikke logget ind =============================
+if (!isset($_SESSION['user']))
+{
+?>
+<form method="post" action="">
+<input name="username" type="text" id="username" value="Brugernavn" class="headerlogin_class" onfocus="if (this.value == 'Brugernavn') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Brugernavn';}"/>
+<input name="password" type="password" id="password" value="Password" class="headerlogin_class"/ onfocus="if (this.value == 'Password') {this.value = '';this.type='password';}" onblur="if (this.value == '') {this.value = 'Password';this.type='password';}"/>
+<input type="submit" name="Submit" value="Login" class="headerlogin_login" />
+<?php if(isset ($bo)){ echo $bo;} ?>
+</form>
+<?php 
+}
+?>
+
