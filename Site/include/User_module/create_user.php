7b5958c39161d490479f1005444c4a1adb41792c
unknown <emsc@TEC-PC-37.pc-tec.dk>
2015-04-28 08:54:10 +0200

    create user get maine course from db done

--- Site/include/User_module/create_user.php
+++ Site/include/User_module/create_user.php
@@ -1,6 +1,15 @@
 <?php
 session_start();
+//dbconnector:
+    $db_host="192.168.0.4"; // Host name
+    $db_username="c1root"; // Mysql username
+    $db_password="A123linux2013"; // Mysql password
+    $db_name="c1praktikcenter"; // Database name
+    $db_conn = mysqli_connect("$db_host","$db_username","$db_password","$db_name");
+
+ 
 ?>
+
 <head>
     <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
     <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
@@ -43,9 +52,23 @@ if((isset ($_SESSION['user'])) && (isset($_SESSION['create_user']))){
             <td>
                 <select required>
                     <option selected value="">V&aelig;lg Udannelse</option>
+                   
+                    <?php 
+                        $sqlState="select * from edu";
+                        $sql_result = mysqli_query($db_conn, $sqlState) or die (mysqli_error($db_conn));
+                       
+
+                        while ($row = mysqli_fetch_assoc($sql_result)){
+                           echo "<option value='".$row['id']."'>".$row['name']."</option>";
+                        }
+                    ?>
+                    
+                    
+                    <!--
                     <option value="1">Datateknikker specalisering i programmering</option>
                     <option value="1">Datateknikker specalisering i infrastruktur</option>
                     <option value="1">It-Supporter</option>
+                    -->
                 </select>
             </td>
         </tr>
