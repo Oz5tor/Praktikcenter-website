12a636659441bb46e7387ae2dfa68adfb3da9837
unknown <emsc@TEC-PC-37.pc-tec.dk>
2015-04-28 10:09:22 +0200

    create user get inst. done

--- Site/include/User_module/create_user.php
+++ Site/include/User_module/create_user.php
@@ -11,9 +11,9 @@ session_start();
 ?>
 
 <head>
-    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+ <link rel="stylesheet"href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
     <script>
         $(function () {
             $("#datepicker").datepicker();
@@ -27,26 +27,29 @@ if((isset ($_SESSION['user'])) && (isset($_SESSION['create_user']))){
 }
     else {header('location: ../../index.php');} 
 ?>
+
+<h1>Opret ny Elev</h1>
 <form action="#" method="get">
     <table>
         <tr>
             <td>Fornavn: </td>
             <td>
-                <input type="text" min="2" max="40" id="fName" required>
+                <input type="text" min="2" max="40" id="fName" name="fName" required>
             </td>
         </tr>
         <tr>
             <td>Efternavn: </td>
             <td>
-                <input type="text" min="2" max="50" id="lName" required>
+                <input type="text" min="2" max="50" id="lName" name="lName" required>
             </td>
         </tr>
         <tr>
             <td>F&oslash;selsdag: </td>
             <td>
-                <input type="text" id="datepicker" required>
+                <input type="text" id="datepicker" name="datepicker" required pattern="[0-9]{2}/[0-9]{2}/[0-9]{4}" required>
             </td>
         </tr>
+        
         <tr>
             <td>Udannelses: </td>
             <td>
@@ -63,41 +66,78 @@ if((isset ($_SESSION['user'])) && (isset($_SESSION['create_user']))){
                         }
                     ?>
                     
-                    
-                    <!--
-                    <option value="1">Datateknikker specalisering i programmering</option>
-                    <option value="1">Datateknikker specalisering i infrastruktur</option>
-                    <option value="1">It-Supporter</option>
-                    -->
                 </select>
             </td>
         </tr>
+      
+        
+         
+        
+        <tr>
+             <td>Kompetancer: </td>
+            <td>
+                
+                 <checkbox>
+                   
+                    <?php 
+                        $sqlState="select * from skills";
+                        $sql_result = mysqli_query($db_conn, $sqlState) or die (mysqli_error($db_conn));
+                       
+
+                        while ($row = mysqli_fetch_assoc($sql_result)){ ?>
+                         <input type="checkbox" name="skill" value="<?php $row['id']; ?>"> <?php echo $row['skill']; ?><br>
+                      <?php  }
+                    ?>
+                </checkbox>
+            </td>            
+        </tr>
+            
+            <tr>
+             <td>Instruktør: </td>
+            <td>
+                <select required>
+                    <option selected value="">V&aelig;lg Instuktør</option>
+                   
+                    <?php 
+                        $sqlState="Select user.fName, user.lName, user.id From roles Inner Join userRoles On roles.id = userRoles.roleId Inner Join user On user.id = userRoles.userId where roles.id = 2";
+                        $sql_result = mysqli_query($db_conn, $sqlState) or die (mysqli_error($db_conn));
+                       
+
+                        while ($row = mysqli_fetch_assoc($sql_result)){
+                           echo "<option value='".$row['id']."'>".$row['fName']." ".$row['lName']."</option>";
+                        }
+                    ?>
+                </select>
+            </td>            
+        </tr>
+        
+        
         <tr>
             <td>Addresse: </td>
             <td>
-                <input type="text" id="address" max="100" required>
+                <input type="text" id="address" name="address" max="100" required>
             </td>
         </tr>
         <tr>
             <td>Email: </td>
             <td>
-                <input type="email" id="email" max="35" required>
+                <input type="email" id="email" name="email" max="35" required>
             </td>
         </tr>
         <tr>
             <td>Telefon: </td>
             <td>
-                <input type="tel" id="phone" min="8" max="8" pattern="[0-9]{8}" required>
+                <input type="tel" id="phone" name="phone" min="8" max="8" pattern="[0-9]{8}" required>
             </td>
         </tr>
         <tr>
             <td colspan="2">
-                <input type="submit" id="create_user" value="Opret bruger">
+                <input type="submit" id="create_user" name="create_user" value="Opret bruger">
             </td>
         </tr>
     </table>
 </form>
 <?php
-echo '<pre>'; print_r($_POST); echo '</pre>'; 
+echo '<pre>'; print_r($_GET); echo '</pre>'; 
 echo '<pre>'; print_r($_SESSION); echo '</pre>'; 
 ?>
