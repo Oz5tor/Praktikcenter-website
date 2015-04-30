b8a5af896cd65df1e042634a280b13df6915d758
Tor Soya <torsoya@gmail.com>
2015-04-30 08:44:08 +0200

    rettede url's
    rettede url's , passer nu med den nye URL controller

--- Site/include/navbox.php
+++ Site/include/navbox.php
@@ -12,35 +12,39 @@
                 } 
                 else
                 {
-                    $nav_image="<img src='" .$nav_row['icon']. "' />"; 
+                    $nav_image="<img src='".$nav_row['icon']."' />"; 
                 } ?>
                 <li>
                     <?php echo $nav_image.' '; echo $nav_row['menu_cat_name']; ?>
                     <?php
                     $sub_nav_sql = "select * from main_menu_subcat Where fk_menu_cat_id = '$nav_cat_id'";
                     $sub_nav_resut = mysqli_query($db_conn, $sub_nav_sql) or die (mysql_error());
-                    while($sub_row = mysqli_fetch_assoc($sub_nav_resut))
+                    // ====================================================
+                    // Page url starting to be created
+                    while($page_row = mysqli_fetch_assoc($sub_nav_resut))
                     {
-                        $sub_cat_id=$sub_row['id'];
-                        if($sub_row['icon']==NULL)
+                        $sub_cat_id=$page_row['id'];
+                        if($page_row['icon']==NULL)
                         {
                             $nav_image='<img src="img/icons/003w.png" />';
                         } 
                         else
                         {
-                            $nav_image="<img src='" .$sub_row['icon']. "' />"; 
+                            $nav_image="<img src='".$page_row['icon']."' />"; 
                         } ?>
                         <ul>
                             <li>
-                                <a href="?p=<?php echo $sub_row['menu_subcat_name']; ?>"><?php echo $nav_image.' '; echo $sub_row['menu_subcat_name']; ?></a>
+                                <a href="?page=<?php echo $page_row['menu_subcat_name']; ?>"><?php echo $nav_image.' '; echo $page_row['menu_subcat_name']; ?></a>
                                 <ul>
                                 <?php
                                 $sub_nav_sql = "select * from main_menu_subsubcat Where fk_menu_subcat_id = '$sub_cat_id'";
                                 $sub_nav_resut = mysqli_query($db_conn, $sub_nav_sql) or die (mysql_error());
+                                // ====================================================
+                                // Subpage url being created
                                 while($sub_row = mysqli_fetch_assoc($sub_nav_resut))
                                 {?>
                                         <li>
-                                            <a href="?p=<?php echo $sub_row['menu_subsubcat_name']; ?>">
+                                            <a href="?page=<?php echo $page_row['menu_subcat_name']; ?>&subpage=<?php echo $sub_row['menu_subsubcat_name']; ?>">
                                                 <?php echo $sub_row['menu_subsubcat_name']; ?>
                                             </a>
                                         </li>                
