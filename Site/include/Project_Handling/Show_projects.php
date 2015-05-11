34273fb4df327a4468f38f86c6e056c978a17445
Emil Schytte Bækgaard <emilschytte@gmail.com>
2015-05-11 09:50:59 +0200

    show projects at 50%

--- Site/include/Project_Handling/Show_projects.php
+++ Site/include/Project_Handling/Show_projects.php
@@ -8,19 +8,36 @@
     $db_conn = mysqli_connect("$db_host","$db_username","$db_password","$db_name");
 ?>
 
-<h1>Projek oversigt</h1>
-
+<h1>Projekt oversigt</h1>
 <?php
 
-$sqlState="select * from projects";
-                        $sql_result = mysqli_query($db_conn, $sqlState) or die (mysqli_error($db_conn));
-                       
+$sqlState = "select name from project";
+$sql_result = $sql_result = mysqli_query($db_conn, $sqlState) or die (mysqli_error($db_conn));
+    
+    while($row = mysqli_fetch_assoc($sql_result)){
+        $proName[]= $row['name']; 
+    }
+        
+$members ="";
+foreach ($proName as $name){
+    
+    $sqlState="Select user.fName, user.lName From project Inner Join userProject On project.id = userProject.projectId Inner Join user On user.id = userProject.userId where       project.name ='$name'";
+    
+    $sql_result = mysqli_query($db_conn, $sqlState) or die (mysqli_error($db_conn));
+    
+    while($row = mysqli_fetch_assoc($sql_result)){
+        $members.= $row['fName']." ".$row['lName'].", "; 
+    }
+                            
+?> 
+    
 
-                        while ($row = mysqli_fetch_assoc($sql_result)){
-                          $members .= $row['fName'].$row['lName']
-                            ;
-                        }
+    <div class="Project">
+        <button type="button" onclick="alert('<?php echo $members?>')">Elever</button>
+        <?php echo $name ?>
+    </div>
 
-
+<?php
+$members =""; }
+unset($name); 
 ?>
-<div class="Project"><button type="button" onclick="alert(<?php echo $members?>)">Elever</button></div>
