bbf3aa0e63337dc2166cf8dc095f0d8b7fd00ea3
Emil Schytte Bækgaard <emilschytte@gmail.com>
2015-05-19 10:46:57 +0200

    createProject working in pro

--- Site/include/Project_Handling/Create_project.php
+++ Site/include/Project_Handling/Create_project.php
@@ -7,8 +7,45 @@
     $db_name="c1praktikcenter"; // Database name
     $db_conn = mysqli_connect("$db_host","$db_username","$db_password","$db_name");
 ?>
+<head>
+      
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script>
+  $(function() {
+    $( "#datepicker" ).datepicker({
+      changeMonth: true,
+      changeYear: true,
+        dateFormat: "yy/mm/dd"
+    });
+  });
+      
+      $(function() {
+    $( "#datepicker2" ).datepicker({
+      changeMonth: true,
+      changeYear: true,
+        dateFormat: "yy/mm/dd"
+    });
+  });
+  </script>
+    
+</head>
 
 <h1>Opret nyt projekt</h1>
+<table>
+<tr>
+        <td>Projekt navn:</td>
+        <td><input type="text" min="2" max="40" id="projectName" name="projectName" required></td>
+    </tr>
+        
+<tr>
+        <td>Projekt Beskrivelse:</td>
+    </tr>
+
+</table>
+ 
 <?php
 
 ?>
