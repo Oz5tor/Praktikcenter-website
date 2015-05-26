21420d9c24ab06a8ace4449924c7fb6ab9e60a01
Emil Schytte Bækgaard <emilschytte@gmail.com>
2015-05-26 09:14:34 +0200

    ændringer grundet database ænding af detaljeret projekt visning

--- Site/include/Project_Handling/project_details.php
+++ Site/include/Project_Handling/project_details.php
@@ -19,14 +19,16 @@ $id = $_GET['projectId'];
 $selctProjects_sql = "SELECT * from project where id=$id";
 $sql_result = $sql_result = mysqli_query($db_conn, $selctProjects_sql) or die (mysqli_error($db_conn));
 
+
 while($row = mysqli_fetch_assoc($sql_result)){
    // udtræk af collonner
+    if($row['end'])
     $name = $row['name'];
     $start = $row['start'];
     $end = $row['end'];
     $proID = $row['id'];
-    $desc = $row['description'];
     $leader = $row['leaderId'];
+    $FK_ProTemp = $row['FK_ProTemp'];
     // henter deltager på projectet
     $student_sql = "Select * From userProject Inner Join user On user.id = userProject.userId where projectid = '$proID'";
     $student_sql_result = mysqli_query($db_conn, $student_sql) or die (mysqli_error($db_conn));
@@ -40,14 +42,26 @@ $findLeaderName_sql_state = "select * from user where id = $leader";
 $findLeaderName_sql_result = mysqli_query($db_conn, $findLeaderName_sql_state) or die (mysqli_error($db_conn));
 while($row = mysqli_fetch_assoc($findLeaderName_sql_result)){       
         $leaderName = $row['fName']." ".$row['lName'];
-   }
 
+
+}
+//Henter projekt beskrivelsen fra Projekt skabelon:
+    
+    
+    $proDescTemp_sql_statement = "select * from proTemp where id = $FK_ProTemp";
+    $proDescTemp_sql_result = mysqli_query($db_conn, $proDescTemp_sql_statement) or die (mysqli_error($db_conn));
+
+    while($row = mysqli_fetch_assoc($proDescTemp_sql_result)){
+        
+        $ProDesc_text = $row['description'];
+        
+    }
 
 // udskrivning af Projektoplysningerne    
     
 echo "<h1>".$name."</h1>";   
 
-echo "<h2>Projektbeskrivelse: </h2><br><i>".$desc."</i><br>";
+echo "<h2>Projektbeskrivelse: </h2><br><i>".$ProDesc_text."</i><br>";
 ?>
 <br><h2>Projekt medlemmer:</h2>
 
@@ -97,14 +111,14 @@ echo "Slut på projekt: ".date('d/m/Y', $end)."<br><br>";
 
 <table>
     <tr>
-        <td><b>Opgave navn</b></td></tr>
+        <td><b>Opgave navn</b></td><td><b>Status i %</b></td></tr>
         <?php 
-            $assignment_sql_state = "Select assignment.name From assignment Inner Join ProjectAssignment On assignment.id = ProjectAssignment.assignemtId Inner Join project On ProjectAssignment.ProjectId = project.id Where project.id = $proID";
+            $assignment_sql_state = "select * from assignment where FK_ProId=$proID";
             $assignment_sql_result = mysqli_query($db_conn, $assignment_sql_state) or die (mysqli_error($db_conn));
             
             while($row = mysqli_fetch_assoc( $assignment_sql_result)){
                 
-                echo"<tr><td>".$row['name']."</td></tr>";
+                echo"<tr><td>".$row['name']."</td><td>".$row['status']."</td></tr>";
             }
         ?>
     
