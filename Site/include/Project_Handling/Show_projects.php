41eba2cb305b39cb96b5afb2595e42d811692ca2
Emil Schytte Bækgaard <emilschytte@gmail.com>
2015-05-26 09:13:14 +0200

    show projects now only shows active projects

--- Site/include/Project_Handling/Show_projects.php
+++ Site/include/Project_Handling/Show_projects.php
@@ -9,11 +9,11 @@
     $db_conn = mysqli_connect("$db_host","$db_username","$db_password","$db_name");
 ?>
 
-<h1>Projekt oversigt</h1>
+<h1>Projekt oversigt over aktive projekter</h1>
 
 <fieldset>
 <?php
-
+$today = getdate();
 
 $selctProjects_sql = "SELECT * from project";
 $sql_result = $sql_result = mysqli_query($db_conn, $selctProjects_sql) or die (mysqli_error($db_conn));
@@ -30,7 +30,7 @@ while($row = mysqli_fetch_assoc($sql_result)){
         $members[] = $student['fName'].' '.$student['lName'].', ';
     }
     // udskrivning af selve projecterne
-    ?>
+    ?><?php if($today[0]<$end){?>
     <div id= "Project" class="Project">
        <fieldset>
         
@@ -43,7 +43,7 @@ while($row = mysqli_fetch_assoc($sql_result)){
     <?php
     // nulstiller arrayet så det er klart til næste project
     unset($members);
-}
+}}
 ?>
 </fieldset>
 
