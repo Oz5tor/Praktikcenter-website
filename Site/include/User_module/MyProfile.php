619a7253cf4f5329af0910a913fff1d4915843b7
Tor Soya <torsoya@gmail.com>
2015-05-28 14:31:36 +0200

    user profile
    user profile now shows the user depending on what id there is given from
    the url.
    
    projects list, so far it takes all projects the user is on even if the
    project is done, might be seperated to completed and ongoin/up commin
    projects, that way the user can see what the plan is for the futhure

--- Site/include/User_module/MyProfile.php
+++ Site/include/User_module/MyProfile.php
@@ -1,8 +1,10 @@
 <?php
-$user = mysqli_real_escape_string($db_conn,strip_tags($_GET['id']));
-$user_sql = "Select * From user where id = '$user'";
+$id = mysqli_real_escape_string($db_conn,strip_tags($_GET['id']));
+$user_sql = "Select * From user where id='$id'";
 $user_result = mysqli_query($db_conn, $user_sql) or die (mysqli_error($db_conn));
-$user_row = mysqli_num_rows($user_result); 
+$user_row = mysqli_fetch_assoc($user_result);
+$eduInt = $user_row['edu'];
+// ====================
 ?>
 
 <style type="text/css">
@@ -17,34 +19,51 @@ $user_row = mysqli_num_rows($user_result);
 }
 .userinfo{
     min-width:400px;
+    max-width:620px;
     float:left;
 }
 
 
 </style>
-
-<div style="border:1px solid black; height:500px;">
-
+<div>
     <div id="baseinfo" class="borderbottom">
-        <img class="profilepic" src="img/profile/nopic.png"/>
-        <div class="userinfo">
-            <table>
-                <tr>
-                    <td>Tor Soya</td>
-                    <td>Webmaster</td>
-                </tr>
-                <tr>
-                    <td colspan="2">Datateknikker - Speciale Programmering</td>
-                </tr>
-                <tr>
-                    <td>kage</td>
-                    <td>mand</td>
-                </tr>
-            </table>
-        </div>     
-        <span style="display:block; clear:both;"></span>
+        <?php include_once('include/User_module/UserBaseInfo.php'); ?>
     </div>
+    <br>
     
-    <hr>
-    
+        <?php
+        $projects_sql = "Select * From userProject Inner Join project On project.id = userProject.projectId where userId = $id order by start ASC";
+        $projects_result = mysqli_query($db_conn, $projects_sql) or die (mysqli_error($db_conn));
+        while ($projects_row = mysqli_fetch_assoc($projects_result)){
+            $name = $projects_row['name'];
+            $start = $projects_row['start'];
+            $end = $projects_row['end'];
+            $proID = $projects_row['id'];
+        ?>
+            <div style="margin-left:5px;margin-right:5px; margin-bottom:5px;width:365px; border: 1px solid black; float:left;">
+                <a href="?administration=Projekt Oversigt&projectId=<?php echo $proID; ?>"><?php echo $name; ?></a><hr>
+                <div>
+                    <div style="float:left; border-right:1px solid black; padding:5px;">
+                        <div style="float:left;">
+                            <?php $startDate = gmdate("d-m-Y",$start); $endDate = gmdate("d-m-Y",$end); ?>
+                            <?php echo "Start dato: ".$startDate;?>
+                        </div>
+                        <div>
+                            <?php echo " Slut dato: ".$endDate?>
+                        </div>
+                    </div>
+                    <div style="float:right; padding:5px;">
+                        Dage tilbage: <?php $date = date_create();
+                        $currentDate = date_timestamp_get($date);
+                        $dayLeft = $end - $currentDate;
+                        $PrintDLeft = $dayLeft / 86400 % 2200;
+                        echo $PrintDLeft; ?>
+                    </div>
+                    
+                </div>
+    </div>
+        <?php
+        }
+        ?>
+    <span style="display:block; clear:both;"></span>
 </div>
---
+++ Site/include/User_module/UserBaseInfo.php
@@ -0,0 +1,51 @@
+<img class="profilepic" src="img/profile/nopic.png"/>
+        <div class="userinfo">
+            <table>
+                <tr>
+                    <td><b>Uddannelse: </b></td>
+                    <?php
+                        $edu_sql = "Select * From edu where id = '$eduInt'";
+                        $edu_result = mysqli_query($db_conn, $edu_sql) or die (mysqli_error($db_conn));
+                        $edu_row = mysqli_fetch_assoc($edu_result); 
+                    ?>
+                    <td><?php echo $edu_row['name']; ?></td>
+                </tr>
+                <tr>
+                    <td><b>Funktion: </b></td>
+                    <?php 
+                         $role_sql ="Select * From user
+                                        Inner Join userRoles On user.id = userRoles.userId 
+                                        Inner Join roles On roles.id = userRoles.roleId
+                                        Where userid ='$id' Order By roleRank ASC";
+                        $role_result = mysqli_query($db_conn, $role_sql) or die (mysqli_error($db_conn));
+                        $role_row = mysqli_fetch_assoc($role_result); 
+                        ?>
+                    <td><?php echo $role_row['name'] ?></td>
+                </tr>
+                <tr>
+                    <td><b>Navn: </b></td>
+                    <td><?php echo $user_row['fName'].' '. $user_row['lName'] ; ?></td>
+                </tr>
+                <tr>
+                    <?php 
+                    // number of days the user has left
+                    $date = date_create();
+                    $currentDate = date_timestamp_get($date);
+                    $dayLeft = $user_row['eduEnd'] - $currentDate;
+                    $PrintDLeft = $dayLeft / 86400 % 2200;  
+                    // ====================
+                    ?>
+                    <td><b>Tid Tilbage :</b></td>
+                    <td><?php echo $PrintDLeft;?> Dage</td>
+                </tr>
+                <tr>
+                    <td><b>Skills: </b></td>
+                    <?php
+                        $skill_sql = "Select * From userSkills Inner Join skills On skills.id = userSkills.skillId where userId =$id";
+                        $skill_result = mysqli_query($db_conn, $skill_sql) or die (mysqli_error($db_conn)); 
+                    ?>
+                    <td><?php while ($skill_row = mysqli_fetch_assoc($skill_result)){echo '#'.$skill_row['skill'].' ';} ?></td>
+                </tr>
+            </table>
+        </div>
+        <span style="display:block; clear:both;"></span>
