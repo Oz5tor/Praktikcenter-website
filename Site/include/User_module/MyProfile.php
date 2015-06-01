8b09599299bf589de0bc84e92a88c96c7a4f3c51
Tor Soya <torsoya@gmail.com>
2015-06-01 14:24:19 +0200

    projects on profile
    curruent and exprided project the user have colaberated in is now shown
    on the users profile

--- Site/include/User_module/MyProfile.php
+++ Site/include/User_module/MyProfile.php
@@ -4,6 +4,7 @@ $user_sql = "Select * From user where id='$id'";
 $user_result = mysqli_query($db_conn, $user_sql) or die (mysqli_error($db_conn));
 $user_row = mysqli_fetch_assoc($user_result);
 $eduInt = $user_row['edu'];
+$tempt_time = time();
 // ====================
 ?>
 
@@ -22,48 +23,39 @@ $eduInt = $user_row['edu'];
     max-width:620px;
     float:left;
 }
+#projectsholder{
+    margin-left:5px;
+    margin-right:5px;
+    margin-bottom:5px;
+    width:365px; 
+    border:1px solid black;
+    float:left;
+}
+.projectStartEnd {
+    float:left; 
+    border-right:1px solid black;
+    padding:5px;
+}
+.daysleft{
+    float:left;
+    padding:5px;
+    width:45%;
+    height:45px;
+    vertical-align:middle;
+}
+.textCenter{
+    text-align:center;
+}
 
 
 </style>
 <div>
-    <div id="baseinfo" class="borderbottom">
+    <div id="baseinfo" class="">
         <?php include_once('include/User_module/UserBaseInfo.php'); ?>
     </div>
-    <br>
-    
-        <?php
-        $projects_sql = "Select * From userProject Inner Join project On project.id = userProject.projectId where userId = $id order by start ASC";
-        $projects_result = mysqli_query($db_conn, $projects_sql) or die (mysqli_error($db_conn));
-        while ($projects_row = mysqli_fetch_assoc($projects_result)){
-            $name = $projects_row['name'];
-            $start = $projects_row['start'];
-            $end = $projects_row['end'];
-            $proID = $projects_row['id'];
-        ?>
-            <div style="margin-left:5px;margin-right:5px; margin-bottom:5px;width:365px; border: 1px solid black; float:left;">
-                <a href="?administration=Projekt Oversigt&projectId=<?php echo $proID; ?>"><?php echo $name; ?></a><hr>
-                <div>
-                    <div style="float:left; border-right:1px solid black; padding:5px;">
-                        <div style="float:left;">
-                            <?php $startDate = gmdate("d-m-Y",$start); $endDate = gmdate("d-m-Y",$end); ?>
-                            <?php echo "Start dato: ".$startDate;?>
-                        </div>
-                        <div>
-                            <?php echo " Slut dato: ".$endDate?>
-                        </div>
-                    </div>
-                    <div style="float:right; padding:5px;">
-                        Dage tilbage: <?php $date = date_create();
-                        $currentDate = date_timestamp_get($date);
-                        $dayLeft = $end - $currentDate;
-                        $PrintDLeft = $dayLeft / 86400 % 2200;
-                        echo $PrintDLeft; ?>
-                    </div>
-                    
-                </div>
-    </div>
-        <?php
-        }
-        ?>
-    <span style="display:block; clear:both;"></span>
+    <hr>
+    <?php include_once('include/User_module/currunt_projects.php'); ?>
+    <hr>
+    <?php include_once('include/User_module/expride_projects.php'); ?>
+    <hr>
 </div>
---
+++ Site/include/User_module/expride_projects.php
@@ -0,0 +1,71 @@
+<div class="textCenter">
+    <p><h3>Afsluttede/ Udløbet Projektor</h3><p>
+</div>
+<?php
+        $projects_sql = "Select * From userProject 
+                            Inner Join project On project.id = userProject.projectId 
+                            where userId = $id && end < $tempt_time order by start ASC";
+        $projects_result = mysqli_query($db_conn, $projects_sql) or die (mysqli_error($db_conn));
+        $temp_any = mysqli_num_rows($projects_result);
+
+
+if($temp_any >= 1){
+    while ($projects_row = mysqli_fetch_assoc($projects_result)){
+            $name = $projects_row['name'];
+            $start = $projects_row['start'];
+            $end = $projects_row['end'];
+            $proID = $projects_row['id'];
+        ?>
+        <a href="?administration=Projekt Oversigt&projectId=<?php echo $proID; ?>">
+            <div id="projectsholder">
+                <div class="textCenter borderbottom"><b><p><?php echo $name; ?></p></b></div>
+                <div>
+                    <div class="projectStartEnd">
+                        <div style="float:left;">
+                            <table>
+                                <tr>
+                                    <td>
+                                        <?php $startDate = gmdate("d-m-Y",$start); $endDate = gmdate("d-m-Y",$end); ?>
+                                        <?php echo "Start dato: "?>
+                                    </td>
+                                    <td><?php echo $startDate;?></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <?php echo "Slut dato: "?>
+                                    </td>
+                                    <td>
+                                        <?php echo $endDate?>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="daysleft">
+                        <table>
+                            <tr>
+                                <td>
+                                    Dage tilbage: <?php $date = date_create();
+                                    $currentDate = date_timestamp_get($date);
+                                    $dayLeft = $end - $currentDate;
+                                    $PrintDLeft = $dayLeft / 86400 % 2200;
+                                    echo $PrintDLeft; ?>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </a>
+        <?php
+        }
+        ?>
+    <span style="display:block; clear:both;"></span>
+<?php
+}
+else{
+    ?>
+    <div class="textCenter"><p><b><?php echo $user_row['fName'].' '. $user_row['lName'].' Har ikke nogen Afsluttede/ Udløbet Projektor'; ?></b></p></div>
+<?php
+}
+        
