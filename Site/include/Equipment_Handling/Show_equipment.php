9b453effda7a19608841933d5a9488c3fca0adc5
Tor Soya <torsoya@gmail.com>
2015-06-03 09:40:41 +0200

    beutifiede the show equipment list
    sugget taht the pager from news.php get adde to prevent maga long pages
    in the further

--- Site/include/Equipment_Handling/Show_equipment.php
+++ Site/include/Equipment_Handling/Show_equipment.php
@@ -1,147 +1,129 @@
-<?php
-
-//dbconnector:
-    $db_host="192.168.0.4"; // Host name
-    $db_username="c1root"; // Mysql username
-    $db_password="A123linux2013"; // Mysql password
-    $db_name="c1praktikcenter"; // Database name
-    $db_conn = mysqli_connect("$db_host","$db_username","$db_password","$db_name");
-?>
-
-<?php
-
-    $sqlState="select * from equipment";
-    $sql_result = mysqli_query($db_conn, $sqlState) or die (mysqli_error($db_conn));
-?>
-
-<h1>Udstyrs Oversigt</h1>
-<form action="" method="post">
-    <table>
-        <tr>
-            <td>Type Udstyr: </td>
-            <td>
-                <select required name="eqType">
-                    <option selected name="eqType" value="eqType">V&aelig;lg</option>
-                   
-                    <?php 
-                        $sqlState="select * from eqType";
-                        $sql_tyOption = mysqli_query($db_conn, $sqlState) or die (mysqli_error($db_conn));
-                       
-
-                        while ($row = mysqli_fetch_assoc($sql_tyOption)){
-                           echo "<option value='".$row['id']."'>".$row['name']."</option>";
-                        }
-                    ?>
-                    
-                </select>
-                
-            </td>
-        </tr>
-        <tr>
-            <td>Producent: </td>
-            <td>
-                <select required name="producent">
-                    <option selected name="producent" value="producent">V&aelig;lg</option>
-                   
-                    <?php 
-                        $sqlState="select * from producent";
-                        $sql_proOption = mysqli_query($db_conn, $sqlState) or die (mysqli_error($db_conn));
-                       
-
-                        while ($row = mysqli_fetch_assoc($sql_proOption)){
-                           echo "<option value='".$row['id']."'>".$row['name']."</option>";
-                        }
-                    ?>
-                    
-                </select>
-                
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <input type="submit" id="show_info" name="show_info" value="Find">
-            </td>
-        </tr>
-    </table>
-</form>
-    <?php 
-    
-if(isset($_POST['show_info'])){
-        $eqType = $_POST['eqType'];
-        $producent = $_POST['producent'];
-    
-    if ($eqType == 'eqType'){
-        $sqlState="select * from equipment where fk_prodId = '$producent'"; //WIP
-        $sql_result = mysqli_query($db_conn, $sqlState) or die (mysqli_error($db_conn));
-        $sql_row = mysqli_num_rows($sql_result);
-    } 
-    elseif ($producent == 'producent') {
-        $sqlState="select * from equipment where fk_eqTypeId = '$eqType'"; //WIP
-        $sql_result = mysqli_query($db_conn, $sqlState) or die (mysqli_error($db_conn));
-        $sql_row = mysqli_num_rows($sql_result);
-    } 
-    else{
-        $sqlState="select * from equipment where fk_prodId = '$producent' AND fk_eqTypeId = '$eqType'"; //WIP
-        $sql_result = mysqli_query($db_conn, $sqlState) or die (mysqli_error($db_conn));
-        $sql_row = mysqli_num_rows($sql_result);
-    }
-    if ($sql_row >=1)
-    {
-    ?>
-        <table border="1">
-        <tr> 
-            <td style="padding:10px; text-align:center">Navn </td>
-            <td style="padding:10px; text-align:center">SN</td>
-            <td style="padding:10px; text-align:center">Producent</td>
-            <td style="padding:10px; text-align:center">Type</td>
-            <td style="padding:10px; text-align:center">Specs</td>
-        </tr>
-        <?php
-        while ($row = mysqli_fetch_assoc($sql_result))
-        {
-        ?>
-            <tr>   
-            <td>
-            <?php echo $row['name']; ?>
-            </td>
-            <td>
-            <?php echo $row['sn']; ?>
-            </td>
-            <td>
-            <?php 
-            $proId = $row['fk_prodId'];
-            $prod_sqlState="select * from producent where id = '$proId'";
-            $prod_sql_proId = mysqli_query($db_conn, $prod_sqlState) or die (mysqli_error($db_conn));
-            $prod_row = mysqli_fetch_assoc($prod_sql_proId);
-            echo $prod_row['name']; ?>
-            </td>
-            <td>
-            <?php
-            $tyId = $row['fk_eqTypeId'];
-            $ty_sqlState="select * from eqType where id = '$tyId'";
-            $ty_sql_tyId = mysqli_query($db_conn, $ty_sqlState) or die (mysqli_error($db_conn));
-            $ty_row = mysqli_fetch_assoc($ty_sql_tyId);
-            echo $ty_row['name']; ?>
-            </td>
-            <td>
-            <?php echo $row['spec']; ?>
-            </td>
-            </tr>
-        <?php
-          }   ?>
-        </table>
-    <?php
-    }  else{   ?>
-    <tr>
-        <td>Søgningen gav ingen resultater...</td>
-    </tr>
-    <?php
-    }   
+<style type="text/css">
+.centerText{
+    text-align: center;
 }
-        
-?>
-<?php
-echo '<pre>';
-print_r($_POST);
-echo '</pre>';
-?>
+.even{
+    background-color:lightgray;
+}
+.uneven{
+    background-color:darkgray;
+}
+</style>
+<div>
+    <div class="centerText"><h2>Praktikcenterts udstyrs liste</h2></div>
+    <hr>
+    <?php
+        // Fetch all equipment types
+        $sql_equipment_type="select * from eqType";
+        $equipment_type_result = mysqli_query($db_conn, $sql_equipment_type) or die (mysqli_error($db_conn));
+        // Fetch all producents
+        $sql_producent="select * from producent ";
+        $producent_result = mysqli_query($db_conn, $sql_producent) or die (mysqli_error($db_conn));
+        // ===============================================================
+    ?>
+    <div class="centerText">
+        <form method="post">
+        Type: 
+        <select required name="eqType">
+            <option selected name="eqType" value="eqType">V&aelig;lg</option>
+            <?php
+                while ($equipment_type = mysqli_fetch_assoc($equipment_type_result)){
+                   echo "<option value='".$equipment_type['id']."'>".$equipment_type['name']."</option>";
+                }
+            ?>
+        </select>
+        Producent: 
+        <select required name="producent">
+            <option selected name="producent" value="producent">V&aelig;lg</option>
+            <?php 
+                while ($producent_list_row = mysqli_fetch_assoc($producent_result)){
+                   echo "<option value='".$producent_list_row['id']."'>".$producent_list_row['name']."</option>";
+                }
+            ?>
+        </select>
+        <input type="submit" id="show_info" name="show_info" value="Find">  
+    </form>
+    </div>
+    <hr>
+    <table border="0" width="755">
+        <tr class="uneven">
+        <th align="center">SN</th>
+        <th align="center">Producent</th>
+        <th align="center">Type</th>
+        <th align="center">Beskrivelse</th>
+        <th align="center">Lokation</th>
+        </tr>
+        <?php
+        if(isset($_POST['show_info'])){
+            $eqType = $_POST['eqType'];
+            $producent = $_POST['producent'];
+            // if producent and priduct type isset
+            if ($producent != 'producent' && $eqType != 'eqType'){
+                $Equip_sql="select eqType.name,
+                                  producent.name As name1,
+                                  equipment.spec,
+                                  equipment.name As name2,
+                                  equipment.fk_prodId,
+                                  equipment.sn From equipment
+                                    Inner Join eqType On eqType.id = equipment.fk_eqTypeId 
+                                    Inner Join producent On producent.id = equipment.fk_prodId
+                                    where fk_prodId = '$producent' AND fk_eqTypeId = '$eqType'"; //WIP
+            } 
+            // if producent isset
+            elseif ($producent != 'producent') {
+                $Equip_sql="select eqType.name,
+                                  producent.name As name1,
+                                  equipment.spec,
+                                  equipment.name As name2,
+                                  equipment.fk_prodId,
+                                  equipment.sn From equipment
+                                    Inner Join eqType On eqType.id = equipment.fk_eqTypeId 
+                                    Inner Join producent On producent.id = equipment.fk_prodId
+                                    where fk_prodId = '$producent'"; //WIP
+            }
+            // if product type isset
+            elseif($eqType != 'eqType'){
+                $Equip_sql="select eqType.name,
+                                  producent.name As name1,
+                                  equipment.spec,
+                                  equipment.name As name2,
+                                  equipment.fk_prodId,
+                                  equipment.sn From equipment
+                                    Inner Join eqType On eqType.id = equipment.fk_eqTypeId 
+                                    Inner Join producent On producent.id = equipment.fk_prodId
+                                    where fk_eqTypeId = '$eqType'"; //WIP
+            }
+        }
+        else{
+            $Equip_sql = "Select  eqType.name,
+                                  producent.name As name1,
+                                  equipment.spec,
+                                  equipment.name As name2,
+                                  equipment.fk_prodId,
+                                  equipment.sn From equipment
+                                    Inner Join eqType On eqType.id = equipment.fk_eqTypeId 
+                                    Inner Join producent On producent.id = equipment.fk_prodId";
+        }
+        $Equip_result = mysqli_query($db_conn, $Equip_sql) or die(mysqli_error($db_conn));
+        $temp_counter = 1;
+        while($Equip_row = mysqli_fetch_assoc($Equip_result)){
+            
+            if($temp_counter%2 == 0){
+                $even_uneven = "uneven";
+            }else{
+                $even_uneven = "even";
+            }
+        ?>
+        <tr class="<?php echo $even_uneven; ?>">
+            <td><?php echo $Equip_row['sn']; ?></td>
+            <td><?php echo $Equip_row['name1']; ?></td>
+            <td><?php echo $Equip_row['name']; ?></td>
+            <td><?php echo $Equip_row['spec']; ?></td>
+            <td><?php //echo $Equip_row['']; ?></td>
+        </tr>
+        <?php
+            $temp_counter++;
+        }
+        ?>
+    </table>
+</div>
